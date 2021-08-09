65611bed13a71e59d85796a39f1fad04257dd383
Nima Weatherly <pompyn@live.com>
2021-08-09 14:33:04 -0500

    add superuser, register Author and Recipe, Add helper to model

--- box/admin.py
+++ box/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from box.models import Author, Recipe
 # Register your models here.
+admin.site.register(Author)
+admin.site.register(Recipe)
--- box/models.py
+++ box/models.py
@@ -6,6 +6,10 @@ class Author(models.Model):
     name = models.CharField(max_length=50)
     bio = models.TextField(null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=50)
