9ae5981fa530d7cb28ba1e2c77625d2515aecd1d
Nima Weatherly <pompyn@live.com>
2021-08-09 18:13:53 -0500

    add index, recipe_detail, author_detail, li for all recipes, display on homepage

--- box/models.py
+++ box/models.py
@@ -10,10 +10,12 @@ class Author(models.Model):
         return self.name
 
 
-
 class Recipe(models.Model):
     title = models.CharField(max_length=50)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     descripton = models.TextField(null=False, blank=False)
     time_required = models.CharField(max_length=30)
     instructions = models.TextField(null=False, blank=False)
+
+    def __str__(self):
+        return self.title
--- box/views.py
+++ box/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from box.models import Recipe, Author
 
 # Create your views here.
+
+
+def index_view(request):
+    recipes = Recipe.objects.all()
+    return render(request, 'index.html', {'recipes': recipes})
+
+
